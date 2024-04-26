7626ea2532e3c243062b50f388c7c173da08c8c5
Shay Axelrod <Axelrod.Shay@gmail.com>
2024-04-26 22:07:27 +0300

    imdb: fixed scraping for the year

--- scripts/imdb.js
+++ scripts/imdb.js
@@ -1,35 +1,23 @@
-
-console.log("running")
-
-function checkPageType(titleSection) {
-    const list_elem = titleSection.querySelector('ul');
-    const children = list_elem.children.length;
-    const isSeries = children > 3 ? true : false;
-    return isSeries;
-}
-
 const titleSection = document.querySelector('h1').parentElement;
-const isSeries = checkPageType(titleSection);
 
 const title_elem = titleSection.querySelector('h1');
 let title = "NoTitle";
 if (title_elem)
     title = removeSpecialChars(title_elem.textContent);
-console.log(`title: ${title}`);
 
 let year_elem = titleSection.querySelector('ul').firstElementChild;
-if (isSeries)
+while (!year_elem.textContent.match(/\b\d{4}(?:-\d{4})?\b/g)) {
     year_elem = year_elem.nextElementSibling;
+}
 year = "0000";
 if (year_elem)
     year = year_elem.textContent.split('–')[0];
-console.log(`year: ${year}`);
 
 const id_elem = year_elem.querySelector('a');
 id = "tt00";
 if (id)
     id = id_elem.getAttribute("href").match(/tt\d+/)[0];
-console.log(`id: ${id}`);
 
 const content = plexStyle(title, year, `imdb-${id}`);
-const plex_elem = createPlexElement(document);
+const plex_elem = createPlexElement(document, content);
+title_elem.insertAdjacentElement('afterend', plex_elem);
