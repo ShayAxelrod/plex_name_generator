9ed64564ac5a7d9670e49ad84b1521e982331b84
Shay Axelrod <Axelrod.Shay@gmail.com>
2024-04-29 21:07:50 +0300

    v1.9 cosmetics

--- scripts/imdb.js
+++ scripts/imdb.js
@@ -1,22 +1,14 @@
 const titleSection = document.querySelector('h1').parentElement;
 
 const title_elem = titleSection.querySelector('h1');
-let title = "NoTitle";
-if (title_elem)
-    title = removeSpecialChars(title_elem.textContent);
+const title = removeSpecialChars(title_elem.textContent);
 
 let year_elem = titleSection.querySelector('ul').firstElementChild;
 while (!year_elem.textContent.match(/\b\d{4}(?:-\d{4})?\b/g)) {
     year_elem = year_elem.nextElementSibling;
 }
-year = "0000";
-if (year_elem)
-    year = year_elem.textContent.split('–')[0];
-
-const id_elem = year_elem.querySelector('a');
-id = "tt00";
-if (id)
-    id = id_elem.getAttribute("href").match(/tt\d+/)[0];
+const year = year_elem.textContent.split('–')[0];
+const id = year_elem.querySelector('a').getAttribute("href").match(/tt\d+/)[0];
 
 const content = plexStyle(title, year, `imdb-${id}`);
 const plex_elem = createPlexElement(document, content);
